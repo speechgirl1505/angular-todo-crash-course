970b05c0bd08517a249486ea389c3f8c2a81700e
Kala Elam <speechgirl1505@gmail.com>
2020-06-03 15:20:00 -0500

    deletes all not one

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -17,4 +17,17 @@ export class TodosComponent implements OnInit {
       this.todos = todos;
     });
   }
+
+  deleteTodo(todo: Todo) {
+    //remove from ui
+    this.todos = this.todos.filter((t) => t.id != todo.id);
+    //remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe((todo) => {
+      this.todos.push(todo);
+    });
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -24,6 +24,17 @@ export class TodoService {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
+  //delete todo
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  //add todo
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
+
   //toggle completed
   toggleCompleted(todo: Todo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`;
